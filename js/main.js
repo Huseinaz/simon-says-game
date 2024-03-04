d88144a7c3ec947d5d280dad6bdfe52d1906cb7f
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-03-04 09:04:21 +0200

    Add Variables

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const colors = ["red", "green", "blue", "yellow"];
+let computerColor = [];
+let userColors = [];
+let level = 0;
+let highScore = 0;
+
+const board = document.querySelector(".board");
+const highScoreLabel = document.getElementById("high-score");
+const levelLabel = document.getElementById("level");
+const playBtn = document.getElementById("play");
